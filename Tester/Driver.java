f0fdfbb892f3555f337fd4fb574bac21ed519785
edwardUL99 <47575833+edwardUL99@users.noreply.github.com>
2019-03-13 13:45:31 +0000

    Update Driver.java in Tester

--- Tester/Driver.java
+++ Tester/Driver.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Driver {
 	public static void main(String[] args) {
-		WordSearchPuzzle test = new WordSearchPuzzle("words.txt", 3, 1, 5);
+		WordSearchPuzzle test = new WordSearchPuzzle("words.txt", 4, 1, 5);
 		System.out.println(test.getWordSearchList());
 	}
 }
